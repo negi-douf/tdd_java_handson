ab89afa6d56dd4df5e773385c6087a0f17f2a03c
Koya Hirata <atarih.ayuok@gmail.com>
2023-09-19 22:29:11 +0900

    currencyを Moneyに引き上げる

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -3,13 +3,10 @@ package money;
 class Dollar extends Money {
 	Dollar(int amount) {
 		this.amount = amount;
+		currency = "USD";
 	}
 	
 	Money times(int multiplier) {
 		return new Dollar(amount * multiplier);
-	}
-
-	String currency() {
-		return "USD";
 	}
 }
--- src/money/Franc.java
+++ src/money/Franc.java
@@ -3,13 +3,10 @@ package money;
 class Franc extends Money {
 	Franc(int amount) {
 		this.amount = amount;
+		currency = "CHF";
 	}
 	
 	Money times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-
-	String currency() {
-		return "CHF";
 	}
 }
