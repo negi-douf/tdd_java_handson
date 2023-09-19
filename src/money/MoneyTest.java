f6e5b2b788ecb80cfcd144e0ab7a1cbb1df36055
Koya Hirata <atarih.ayuok@gmail.com>
2023-09-19 22:38:22 +0900

    Dollarの initを読んでいた箇所を修正

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -17,7 +17,7 @@ class MoneyTest {
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(Money.franc(5).equals(Money.franc(5)));
 		assertFalse(Money.franc(5).equals(Money.franc(6)));
-		assertFalse(Money.franc(5).equals(new Dollar(5)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 	
 	@Test
